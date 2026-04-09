78fcdc3cb327eb23a9960da9f858ae978e488fc8
mega <35856629+megambohpie@users.noreply.github.com>
2026-04-09 12:32:51 +0700

    cek lagi

--- src/views/cv/TechCVFixed.vue
+++ src/views/cv/TechCVFixed.vue
@@ -3,7 +3,7 @@
     <nav class="tech-nav">
       <button @click="goBack" class="tech-btn">
         <span class="btn-icon">←</span>
-        <span>HOME 2</span>
+        <span>HOME 288</span>
       </button>
       <LanguageSwitcher />
     </nav>
